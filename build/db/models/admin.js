29c498e35bd3e5b4406492a07d6d28996758ddfb
Abdullah Essam <abdoessam.2010@gmail.com>
2024-06-02 17:41:11 +0300

    delete build directory

--- build/db/models/admin.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose_1 = require("mongoose");
-const adminsSchema_1 = __importDefault(require("../schemas/users/adminsSchema"));
-exports.default = (0, mongoose_1.model)('admin', adminsSchema_1.default);
--- build/routes/web/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const node_path_1 = require("node:path");
-const router = (0, express_1.Router)();
-router.get('/', (req, res) => {
-    res.status(200).sendFile((0, node_path_1.join)(__dirname, '..', '..', '..', 'public', 'index.html'));
-});
-exports.default = router;
